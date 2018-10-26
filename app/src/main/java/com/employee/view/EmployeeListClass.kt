daa0a899c7c12c7ed4f04f55ffca0f3c994180a3
laxmikant nuchuchi <laxmikant.nuchchi@mindtree.com>
2018-10-26 17:30:40 +0530

    Created Folders for Room and Dao

---
+++ app/src/main/java/com/employee/view/EmployeeListClass.kt
@@ -0,0 +1,56 @@
+package com.employee.view
+
+import android.app.Activity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.employee.R
+import com.mykotlinapp.model.EmpListAdapter
+import com.mykotlinapp.model.Employee
+import com.mykotlinapp.viewmodels.EmployeeViewModel
+
+class EmployeeListClass : AppCompatActivity() {
+
+    val NEW_WORD_ACTIVITY_REQUEST_CODE = 1
+
+    private var mEmpViewModel: EmployeeViewModel?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+       mEmpViewModel = ViewModelProviders.of(this).get(EmployeeViewModel::class.java)
+
+            val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+           val adapter = EmpListAdapter(this)
+           recyclerView?.layoutManager = LinearLayoutManager(this)
+           recyclerView?. adapter = adapter
+
+           mEmpViewModel?.getAllWords()?.observe(this, object : Observer<List<Employee>> {
+               override fun onChanged(t: List<Employee>?) {
+                   adapter.setWords(t!!)
+               }
+           });
+
+           findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+               val intent = Intent(this@EmployeeListClass, MainActivity::class.java)
+               startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE)
+           }
+    }
+
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val employee = Employee(data!!.getStringExtra(MainActivity.EXTRA_NAME),
+                    data.getStringExtra(MainActivity.EXTRA_AGE))
+            mEmpViewModel?.insert(employee)
+        } else {
+        }
+    }
+}
---
+++ app/src/main/java/com/employee/view/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.employee.view
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import com.employee.R
+
+class MainActivity : AppCompatActivity() {
+
+    private var mEditNameView: EditText? = null
+    private var mEditAgeView: EditText? = null
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mEditNameView = findViewById(R.id.name)
+        mEditAgeView = findViewById(R.id.age)
+
+        val button = findViewById<Button>(R.id.add_employee)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(mEditNameView!!.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val employeeName = mEditNameView!!.text.toString()
+                val employeeage = mEditAgeView!!.text.toString()
+
+                replyIntent.putExtra(EXTRA_NAME, employeeName)
+                replyIntent.putExtra(EXTRA_AGE, employeeage)
+
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+
+        val EXTRA_NAME = "name"
+        val EXTRA_AGE = "age"
+
+    }
+}
